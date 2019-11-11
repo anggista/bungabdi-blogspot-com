9bcbc6efc3bcf2b6d40029045d1483f415cc8eb8
anggista <55666072+anggista@users.noreply.github.com>
2019-11-11 10:44:22 +0700

    Create Loading-Staggered-Stair.js

---
+++ Loading-Staggered-Stair.js
@@ -0,0 +1,53 @@
+/*
+Name Plug-in : Loading Staggered Stair
+Penulis : Abdi Syahputra Harahap
+Web : https://bungabdi.blogspot.com
+*/
+
+function lst(TagID, sizefont, color, time) {
+document.getElementById(TagID).innerHTML = `<div id="lb1">
+<div class="loading">
+Loading</div>
+</div>`;
+var loading = document.querySelector(".loading");
+var letters = loading.textContent.split("");
+loading.textContent = "";
+letters.forEach(function (letter, i) {
+    var span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = i / +time+ + "s";
+    loading.append(span);
+});
+
+ var style = document.createElement('style');
+  style.innerHTML =
+ ` 
+@import url(https://fonts.googleapis.com/css?family=Lato);
+#lb1 {
+  text-align: center;
+  margin-top: 5%;
+}
+
+.loading {
+  display: flex;
+  color: white;
+  font-size: `+sizefont+`px;
+  font-family: Lato, sans-serif;
+  text-transform: uppercase;
+}
+.loading span {
+  padding: 0 10px;
+  line-height: 50px;
+  background: `+color+`;
+  animation: bulge 2s infinite;
+}
+
+@keyframes bulge {
+  50% {
+    box-shadow: 0 20px 0 #eee;
+    transform: translateY(-35px);
+  }
+}
+`;
+  document.head.appendChild(style);
+}
